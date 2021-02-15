ec4c6bc9350a3e1be78b3815c9f3184d84e44afc
John Preston <wcerfgba@riseup.net>
2021-02-15 20:03:53 +0000

    Expose barline events

--- arla-core/src/alda/lisp/events/barline.clj
+++ arla-core/src/alda/lisp/events/barline.clj
@@ -1,8 +1,21 @@
 (ns alda.lisp.events.barline
-  (:require [alda.lisp.model.event :refer (update-score*)]))
+  (:require [alda.lisp.model.event :refer (update-score* add-events)]
+            [alda.lisp.score.util     :refer (get-current-instruments)]
+            [alda.lisp.model.records :refer [->AbsoluteOffset]]))
 
 (defmethod update-score* :barline
-  [score _]
+  [{:keys [current-voice] :as score} _]
   "Barlines, at least currently, do nothing when evaluated in alda.lisp."
-  score)
+  (add-events score (map (fn [{:keys [current-offset id]}]
+                           {:offset       (->AbsoluteOffset (- (:offset current-offset) 0.1))
+                            :instrument   id
+                            :volume       0
+                            :track-volume 0
+                            :panning      0
+                            :midi-note    0
+                            :pitch        0
+                            :duration     0
+                            :voice        current-voice
+                            :type :barline})
+                         (get-current-instruments score))))
 
