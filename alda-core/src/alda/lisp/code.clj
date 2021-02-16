0adece58d4428ff3d4771efa71ea9ac85d4afa3c
John Preston <wcerfgba@riseup.net>
2021-02-16 20:42:38 +0000

    Remove alda-core

--- alda-core/src/alda/lisp/code.clj
+++
@@ -1,9 +0,0 @@
-(ns alda.lisp.code
-  (:require [alda.parser :refer (parse-input)]))
-
-(defn alda-code
-  "Attempts to parse a string of text within the context of the current score;
-   if the code parses successfully, the result is one or more events that are
-   spliced into the score."
-  [code]
-  (parse-input code :output :events))
--- alda-core/src/alda/version.clj
+++
@@ -1,6 +0,0 @@
-(ns alda.version
-  (:require [manifest.core :refer (manifest)]))
-
-(def ^:const -version-
-  (or (:alda-version (manifest "alda.Main"))
-      "unknown / development version"))
