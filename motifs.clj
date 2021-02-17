f78383976e07ce0e62c37537c478a44c51969606
John Preston <wcerfgba@riseup.net>
2021-02-17 21:14:56 +0000

    Implement `key-interpretations`

--- motifs.clj
+++ motifs.clj
@@ -1,7 +1,5 @@
 (ns motifs
-  (:require [alda.core :as alda]))
-
-(def play (partial alda.core/alda "play" "-c"))
+  (:require [utils :refer [play key-interpretations]]))
 
 (def m-1
   "piano: o3
@@ -9,8 +7,23 @@
    e g f+ r | d f e r | c+ e d e")
 
 (comment
-  (play m-1))
-
-
-
-(alda.core/->str (alda.core/parse-events! m-1))
+  (play m-1)
+  (key-interpretations m-1)
+  ;; => ("A     1  3  5  6 |  5 b7  6  5 |  3  1  2  3 |  5 b7  6 r |  4 b6  5 r |  3  5  4  5"
+  ;;     "A#    7 b3 b5 b6 | b5  6 b6 b5 | b3  7 b2 b3 | b5  6 b6 r |  3  5 b5 r | b3 b5  3 b5"
+  ;;     "Ab   b2  4 b6 b7 | b6  7 b7 b6 |  4 b2 b3  4 | b6  7 b7 r | b5  6 b6 r |  4 b6 b5 b6"
+  ;;     "B    b7  2  4  5 |  4 b6  5  4 |  2 b7  1  2 |  4 b6  5 r | b3 b5  4 r |  2  4 b3  4"
+  ;;     "Bb    7 b3 b5 b6 | b5  6 b6 b5 | b3  7 b2 b3 | b5  6 b6 r |  3  5 b5 r | b3 b5  3 b5"
+  ;;     "C     6 b2  3 b5 |  3  5 b5  3 | b2  6  7 b2 |  3  5 b5 r |  2  4  3 r | b2  3  2  3"
+  ;;     "C#   b6  1 b3  4 | b3 b5  4 b3 |  1 b6 b7  1 | b3 b5  4 r | b2  3 b3 r |  1 b3 b2 b3"
+  ;;     "D     5  7  2  3 |  2  4  3  2 |  7  5  6  7 |  2  4  3 r |  1 b3  2 r |  7  2  1  2"
+  ;;     "D#   b5 b7 b2 b3 | b2  3 b3 b2 | b7 b5 b6 b7 | b2  3 b3 r |  7  2 b2 r | b7 b2  7 b2"
+  ;;     "Db   b6  1 b3  4 | b3 b5  4 b3 |  1 b6 b7  1 | b3 b5  4 r | b2  3 b3 r |  1 b3 b2 b3"
+  ;;     "E     4  6  1  2 |  1 b3  2  1 |  6  4  5  6 |  1 b3  2 r | b7 b2  1 r |  6  1 b7  1"
+  ;;     "Eb   b5 b7 b2 b3 | b2  3 b3 b2 | b7 b5 b6 b7 | b2  3 b3 r |  7  2 b2 r | b7 b2  7 b2"
+  ;;     "F     3 b6  7 b2 |  7  2 b2  7 | b6  3 b5 b6 |  7  2 b2 r |  6  1  7 r | b6  7  6  7"
+  ;;     "F#   b3  5 b7  1 | b7 b2  1 b7 |  5 b3  4  5 | b7 b2  1 r | b6  7 b7 r |  5 b7 b6 b7"
+  ;;     "G     2 b5  6  7 |  6  1  7  6 | b5  2  3 b5 |  6  1  7 r |  5 b7  6 r | b5  6  5  6"
+  ;;     "G#   b2  4 b6 b7 | b6  7 b7 b6 |  4 b2 b3  4 | b6  7 b7 r | b5  6 b6 r |  4 b6 b5 b6"
+  ;;     "Gb   b3  5 b7  1 | b7 b2  1 b7 |  5 b3  4  5 | b7 b2  1 r | b6  7 b7 r |  5 b7 b6 b7")
+)
--- utils.clj
+++ utils.clj
@@ -1,2 +1,101 @@
 (ns utils
-  (:require))
+  (:require [alda.core :as alda]))
+
+(def play (partial alda/alda "play" "-c"))
+
+(def events alda/parse-events!)
+
+(def ^:private integer->degree
+  {0 "1"
+   1 "b2"
+   2 "2"
+   3 "b3"
+   4 "3"
+   5 "4"
+   6 "b5"
+   7 "5"
+   8 "b6"
+   9 "6"
+   10 "b7"
+   11 "7"})
+
+(def ^:private note->integer
+  {"C" 0
+   "C#" 1
+   "Db" 1
+   "D" 2
+   "D#" 3
+   "Eb" 3
+   "E" 4
+   "F" 5
+   "F#" 6
+   "Gb" 6
+   "G" 7
+   "G#" 8
+   "Ab" 8
+   "A" 9
+   "A#" 10
+   "Bb" 10
+   "B" 11})
+
+(defn- interpret-note-in-key
+  [key note]
+  (let [{{:keys [accidentals letter]} :pitch} note
+        accidental-shift (reduce (fn [acc accidental]
+                                   (+ acc (if (= :sharp accidental)
+                                            1
+                                            -1)))
+                                 0
+                                 accidentals)
+        key-shift (- (note->integer key))
+        note-value (note->integer letter)
+        degree (integer->degree (mod (+ note-value accidental-shift key-shift)
+                                     12))]
+    (-> note
+        (assoc-in [:duration :components] '())
+        (assoc-in [:pitch :accidentals] '())
+        (assoc-in [:pitch :letter] degree))))
+
+(defn- interpret-chord-in-key
+  [key chord]
+  (alda/map->Chord {:events (map (partial interpret-note-in-key key) (:events chord))}))
+
+(defn- interpret-events-in-key
+  [key events]
+  (reduce (fn [events event]
+            (cond (= alda.core.Note (type event))
+                  (conj events (interpret-note-in-key key event))
+
+                  (= alda.core.Chord (type event))
+                  (conj events (interpret-chord-in-key key event))
+
+                  :else
+                  (conj events event)))
+          []
+          events))
+
+(defn- format-note-degree
+  [event]
+  (cond (= alda.core.Note (type event))
+        (update-in event [:pitch :letter] #(format "%2s" %))
+
+        (= alda.core.Chord (type event))
+        (alda/map->Chord {:events (map format-note-degree (:events event))})
+
+        :else
+        event))
+
+(defn key-interpretations
+  [code]
+  (let [events (events code)]
+    (map (fn [key]
+           (format "%-2s   %s" key (->> (interpret-events-in-key key events)
+                                       (filter #(#{alda.core.Note
+                                                   alda.core.Chord
+                                                   alda.core.Barline
+                                                   alda.core.Rest} (type %)))
+                                       (map format-note-degree)
+                                       (alda/->str))))
+         (sort (keys note->integer)))))
+
+(comment (key-interpretations "piano: c/e/g"))
